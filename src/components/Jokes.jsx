6275e09949510908762a9aa49312a62bd6bc48c8
prabhav <prabhavdixit007@gmail.com>
2024-05-03 17:16:43 +0530

    fixed margin

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -8,12 +8,12 @@ const Jokes = () => {
 			{loading ? (
 				<div className="text-xl m-[5rem]">Loading...</div>
 			) : (
-				<div className="grid grid-cols-1 sm:grid-cols-3 gap-6 m-[5rem]">
+				<div className="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:m-[5rem] m-[2rem]">
 					{jokesArray?.map((joke, index) => {
 						return (
 							<div
 								key={index}
-								className="p-[3rem] text-xl shadow-lg flex flex-col rounded-lg bg-gray-50 gap-4">
+								className="p-[2rem] text-xl shadow-lg flex flex-col rounded-lg bg-gray-50">
 								{joke?.setup ? (
 									<div>
 										<div className="text-gray-500">{joke?.setup}</div>
